ec4cf8e788c0f18adbd684145a1c58e385349044
Tomas Fryc <fryc.tomas31@gmail.com>
2017-12-04 21:07:00 +0100

    vypis zakoupenych vstupenek

--- app/Repositories/TicketRepositoryInterface.php
+++ app/Repositories/TicketRepositoryInterface.php
@@ -6,20 +6,11 @@ use Illuminate\Support\Facades\DB;
 
 interface TicketRepositoryInterface {
 
-
-
-//    public function getItemsByIisConcertIdSortedByOrder(int $iisConcertId);
-//
-//    public function insert(int $iis_ticket_typeid, int $iis_userid, $code);
-
     public function checkExistingTicketCode($code);
 
     public function insertGetId(array $data);
 
     public function getItemById(int $id);
-//
-//    public function deleteById(int $concertInterpretId);
-//
-//    public function deleteByConcertId(int $concertId);
 
+    public function getItemsByUserId (int $userId);
 }
--- app/UserItem.php
+++ app/UserItem.php
@@ -57,4 +57,9 @@ class UserItem extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getTicketItemsByUserId()
+    {
+        return iisTicketRepository()->getItemsByUserId($this->getId());
+    }
 }
