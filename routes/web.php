b29a30d9a57b15f8d1eed900ffe1bb0971231716
David <david@illichmann.cz>
2017-12-04 22:59:52 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -71,8 +71,8 @@ Route::get('/interpret/{id}/dislike', 'InterpretController@dislike')->name('inte
 // Login and Register Routes
 Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('/register', 'Auth\RegisterController@register');
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
